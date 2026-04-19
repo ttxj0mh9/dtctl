715acc5a2d8a77757eeec7a70d069e228e5a50af
T. Xu <ttxj0mh9@users.noreply.github.com>
2026-04-19 22:45:52 +0800

    simplify exit code comment, remove stale TODO

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ func main() {
 
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		// Use exit code 2 for usage errors vs 1 for general errors
-		// TODO: differentiate between usage errors (2) and runtime errors (1)
+		// Exit code 1 for all errors for now
+		// See: https://github.com/dynatrace-oss/dtctl/issues for upstream discussion
 		os.Exit(1)
 	}
 }
